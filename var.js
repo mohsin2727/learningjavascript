a639baea963a5333444cbdb1f820f122f2cc1f95
mohsin2727 <mohsinsayyad575@gmail.com>
2026-07-03 17:59:34 +0530

    this is variable related prgrams

---
+++ var.js
@@ -0,0 +1,21 @@
+// Old style
+var a = 10    
+
+// Prferred for non-const
+let b = 20;    
+
+// Preferred for const (cannot be changed)
+const c = 30;  
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+let emailAddress = "mohsinsayyad575@gmail.com";
+const fname = "SinuSayyd";
+console.log(emailAddress);
+console.log(fname);
+emailAddress="sinusayyad2727@gmail.com";
+console.log(emailAddress);
+//var emailAddress = "Sayyad575@gmail.com";
+//console.log(emailAddress);
