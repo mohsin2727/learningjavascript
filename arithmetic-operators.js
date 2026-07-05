56ff124d96aebdebad8613225d58dd501ff80374
mohsin2727 <mohsinsayyad575@gmail.com>
2026-07-05 18:12:15 +0530

    data types javascript program

---
+++ arithmetic-operators.js
@@ -0,0 +1,31 @@
+/*
+arithmetic operator
+const sum = 5 + 3; // Addition
+const diff = 10 - 2; // Subtraction
+const p = 4 * 2; // Multiplication
+const q = 8 / 2; // Division
+console.log(sum, diff, p, q);
+*/
+
+/*
+assignment operator
+let n = 10;
+n += 5;
+n *= 2;
+console.log(n);
+*/
+
+/*
+comparison operator
+console.log(10 > 5);
+console.log(10 === "10");
+*/
+
+/*
+const a = true, b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+*/
+
+
+
